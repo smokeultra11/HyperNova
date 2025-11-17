3d3761a778f2411f4fb3e1028292516d9abeeb9c
smokeultra11 <smokeultra11@gmail.com>
2025-11-17 06:42:53 +0300

    Delete static directory

--- static/js/main.js
+++
@@ -1,6 +0,0 @@
-// Orijinal <script> içindekileri buraya taşı
-let conversation = [];
-// ... tüm fonksiyonlar (sendMessage, toggleTheme, vs.)
-document.addEventListener('DOMContentLoaded', () => {
-    // Init
-});
