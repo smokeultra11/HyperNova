997e269c2a42a88258518136f5828232e84392ac
smokeultra11 <smokeultra11@gmail.com>
2025-11-17 03:39:52 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,6 @@
-
+// Orijinal <script> içindekileri buraya taşı
+let conversation = [];
+// ... tüm fonksiyonlar (sendMessage, toggleTheme, vs.)
+document.addEventListener('DOMContentLoaded', () => {
+    // Init
+});
